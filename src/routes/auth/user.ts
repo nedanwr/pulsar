d3db85a62411e7a58b4a677824059f443725746a
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-14 00:06:09 +0500

    Feat: import & post to createUser controller

---
+++ src/routes/auth/user.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+const router:Router = Router();
+
+// Import Controllers
+import { createUser } from "../../controllers/auth/user";
+
+// @desc        Create User
+// @route       POST /api/v1/user
+router.route("/").post(createUser);
+
+export default router;
