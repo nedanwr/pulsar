aeed8259e9b74c7a3be8542c3a645d74949ca20e
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-13 23:23:25 +0500

    Feat: add logging w/morgan & add body/json parser

--- src/start.ts
+++ src/start.ts
@@ -1,8 +1,25 @@
 import "dotenv/config";
 import express, { Application } from "express";
+import {
+    __prod__,
+    port,
+    env
+} from "./lib/constants";
 
 // Init Express
 const app: Application = express();
+
+// Add logging in dev mode
+if (!__prod__) {
+    app.use(require("morgan")("dev"));
+}
+
+// Body Parser
+app.use(express.urlencoded({
+    extended: true,
+}));
+// JSON Parser
+app.use(express.json());
 
 // Start Server
 app.listen(8080, () => console.log("Server started on port 8080"));
