7a864ba081bdfbc2d64102bec3d7823efce39a68
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-13 23:21:54 +0500

    Feat: define express related variables

---
+++ src/lib/constants.ts
@@ -0,0 +1,6 @@
+// Express
+export const port:number = parseInt(process.env.PORT!) || 3000;
+export const __prod__:boolean = process.env.NODE_ENV === "production";
+export const env = process.env.NODE_ENV || "development";
+export const rateLimit:number = parseInt(process.env.RATE_LIMIT!);
+export const rateLimitDuration:number = parseInt(process.env.RATE_LIMIT_DURATION!);
