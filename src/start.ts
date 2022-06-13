01535b91a83a46848034ae3abe77f5b53c0b3be2
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-13 23:45:31 +0500

    Feat: send welcome msg on GET /

--- src/start.ts
+++ src/start.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import {
     __prod__,
     port,
@@ -20,6 +20,14 @@ app.use(express.urlencoded({
 }));
 // JSON Parser
 app.use(express.json());
+
+// Send welcome message
+app.get("/", (_req:Request, res:Response) => {
+    res.status(200).json({
+        statusCode: 200,
+        message: "Welcome to the Pulsar API"
+    })
+});
 
 // Start Server
 app.listen(port, () => console.log(`Server started in ${env} mode on port ${port}`));
