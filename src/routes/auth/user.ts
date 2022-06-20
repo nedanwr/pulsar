f7a3fce035856b5c333fdf417d6f1d610ac31bf1
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-20 22:12:08 +0500

    Refactor: use @controllers path

--- src/routes/auth/user.ts
+++ src/routes/auth/user.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router:Router = Router();
 
 // Import Controllers
-import { createUser } from "../../controllers/auth/user";
+import { createUser } from "@controllers/auth/user";
 
 // @desc        Create User
 // @route       POST /api/v1/user
