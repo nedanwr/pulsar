fdaef341f92c105fcbdbbfdfdb92e563e0389bbc
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-20 22:01:27 +0500

    Feat: create generateAccessToken function

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -1,3 +1,16 @@
+import { sign } from "jsonwebtoken";
+import { jwtSecret } from "./constants";
+
+// Generate access token
+export const generateAccessToken = async (uid: string): Promise<void> => {
+    sign({ uid }, jwtSecret, (err: Error | null, token: string | undefined) => {
+        if (err) {
+            throw err;
+        }
+        return token;
+    });
+}
+
 // Generate a random 4 digit number between 1000 and 9999
 export const generateDiscriminator = (min:number = 1000, max:number = 9999) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
