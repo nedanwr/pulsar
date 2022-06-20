7b3bd854ba93236612292d49119edb85c604e26d
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-20 22:01:59 +0500

    Chore: update repo

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,9 @@
 // Express
-export const port:number = parseInt(process.env.PORT!) || 3000;
-export const __prod__:boolean = process.env.NODE_ENV === "production";
+export const port: number = parseInt(process.env.PORT!) || 3000;
+export const __prod__: boolean = process.env.NODE_ENV === "production";
 export const env = process.env.NODE_ENV || "development";
-export const rateLimit:number = parseInt(process.env.RATE_LIMIT!);
-export const rateLimitDuration:number = parseInt(process.env.RATE_LIMIT_DURATION!);
+export const rateLimit: number = parseInt(process.env.RATE_LIMIT!);
+export const rateLimitDuration: number = parseInt(process.env.RATE_LIMIT_DURATION!);
 
 // JWT
 export const jwtSecret: string = process.env.JWT_SECRET || "";
