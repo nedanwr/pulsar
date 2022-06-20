f9f9b92638423c4af295d2cc34ffa004182facce
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-20 22:01:03 +0500

    Feat: define jwtSecret variable

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,3 +4,6 @@ export const __prod__:boolean = process.env.NODE_ENV === "production";
 export const env = process.env.NODE_ENV || "development";
 export const rateLimit:number = parseInt(process.env.RATE_LIMIT!);
 export const rateLimitDuration:number = parseInt(process.env.RATE_LIMIT_DURATION!);
+
+// JWT
+export const jwtSecret: string = process.env.JWT_SECRET || "";
