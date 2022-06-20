2323471275b1a89d48091473796073b19cbe795d
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-20 22:02:10 +0500

    Chore: update repo

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -12,6 +12,6 @@ export const generateAccessToken = async (uid: string): Promise<void> => {
 }
 
 // Generate a random 4 digit number between 1000 and 9999
-export const generateDiscriminator = (min:number = 1000, max:number = 9999) => {
+export const generateDiscriminator = (min: number = 1000, max: number = 9999) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
