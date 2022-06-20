1003bc60aa737734d4bd38bf3a97a365d6fb1703
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-20 22:15:49 +0500

    Chore: update repo

--- src/routes/auth/user.ts
+++ src/routes/auth/user.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
-const router:Router = Router();
+const router: Router = Router();
 
-// Import Controllers
+// Import Controller
 import { createUser } from "@controllers/auth/user";
 
 // @desc        Create User
