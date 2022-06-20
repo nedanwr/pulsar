377661d4b84d1cc3772df400341c067ae4c6ddb3
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-20 22:16:09 +0500

    Feat: import & use login route

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,8 +3,10 @@ const router:Router = Router();
 
 // Import Routes
 import registerRoute from "@routes/auth/user";
+import loginRoute from "@routes/auth";
 
 // Use Routes
 router.use("/auth/user", registerRoute);
+router.use("/auth", loginRoute);
 
 export default router;
