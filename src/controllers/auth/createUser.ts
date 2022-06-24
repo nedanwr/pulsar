80b77156bb51dd0d685f2b5b66ab15b40868fbc5
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-24 14:00:13 +0200

    Feat: move to own file

---
+++ src/controllers/auth/createUser.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from "express";
+import Joi, { ValidationResult } from "joi";
+import { prisma } from "@prisma";
+import { generateDiscriminator } from "@lib/user";
+import { hashPassword } from "@lib/auth";
+
+const createUser = async (req: Request, res: Response) => {
+    // Validate the request body
+    const schema = Joi.object({
+        email: Joi.string()
+            .email()
+            .required(),
+
+        username: Joi.string()
+            .min(3)
+            .max(16)
+            .required(),
+
+        password: Joi.string()
+            .min(8)
+            .max(256)
+            .required(),
+    });
+
+    const result: ValidationResult<unknown> = schema.validate(req.body);
+
+    // If the request body is invalid, return a 400 error
+    if (result.error) {
+        return res.status(400).json({
+            statusCode: 400,
+            error: "Bad Request",
+            message: result.error.details[0].message,
+        });
+    }
+
+    // Create a new user
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            username: req.body.username,
+            discriminator: generateDiscriminator(),
+            password: await hashPassword(req.body.password),
+            createdAt: new Date().getTime(),
+        }
+    })
+        .catch((error) => {
+            throw error;
+        })
+        .finally(async () => {
+            await prisma.$disconnect();
+            return res.status(201).json({
+                statusCode: 201,
+                message: "User created successfully",
+            });
+        });
+}
+
+export default createUser;
