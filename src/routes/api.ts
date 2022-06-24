e58d1b5e2f40a1ff9970061e34be5c1d467f4117
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-24 14:02:04 +0200

    Feat: import & use usersRoute

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -4,9 +4,11 @@ const router:Router = Router();
 // Import Routes
 import registerRoute from "@routes/auth/user";
 import loginRoute from "@routes/auth";
+import usersRoute from "@routes/users";
 
 // Use Routes
 router.use("/auth/user", registerRoute);
 router.use("/auth", loginRoute);
+router.use("/users", usersRoute);
 
 export default router;
