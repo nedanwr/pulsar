eeeb8cdac257678bcbaab3c7647bc582cd471a79
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-26 12:42:48 +0200

    Feat: config swagger

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,5 +1,13 @@
 import { Router } from "express";
+import { readFileSync } from "fs";
+import swagger from "swagger-ui-express";
 const router:Router = Router();
+
+const swaggerDocument: any = readFileSync("./docs/swagger.json", "utf-8");
+const swaggerOptions: object = {
+    customSiteTitle: "Pulsar API Docs",
+    customCss: ".swagger-ui .topbar { display: none }",
+}
 
 // Import Routes
 import registerRoute from "@routes/auth/user";
@@ -8,6 +16,8 @@ import usersRoute from "@routes/users";
 import serverRoute from "@routes/server";
 
 // Use Routes
+router.use("/docs", swagger.serve);
+router.use("/docs", swagger.setup(JSON.parse(swaggerDocument), swaggerOptions));
 router.use("/auth/user", registerRoute);
 router.use("/auth", loginRoute);
 router.use("/users", usersRoute);
