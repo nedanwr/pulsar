73cce1539aa3f23afa87f66e29b0d8037af157d9
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-27 15:33:12 +0200

    Feat: get specific user & patch current user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,10 +3,18 @@ import { verifyToken } from "../utils/verifyToken";
 const router: Router = Router()
 
 // Import Controllers
-import { getCurrentUser } from "@controllers/users";
+import { getCurrentUser, getUser, modifyCurrentUser } from "@controllers/users";
 
 // @desc        Get Current User
 // @route       /api/v1/users/@me
 router.route("/@me").get(verifyToken, getCurrentUser);
+
+// @desc        Get Specific User
+// @route       /api/v1/users/:uid
+router.route("/:uid").get(verifyToken, getUser);
+
+// @desc        Modify Current User
+// @route       PATCH /api/v1/users/@me
+router.route("/@me").patch(verifyToken, modifyCurrentUser);
 
 export default router;
