923aadbead2f8f4fa4dc21e9b0ffdbc0abf77625
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-28 20:40:38 +0200

    Feat: define maxUploadSize

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,6 +4,7 @@ export const __prod__: boolean = process.env.NODE_ENV === "production";
 export const env = process.env.NODE_ENV || "development";
 export const rateLimit: number = parseInt(process.env.RATE_LIMIT!);
 export const rateLimitDuration: number = parseInt(process.env.RATE_LIMIT_DURATION!);
+export const maxUploadSize: number = parseInt(process.env.MAX_UPLOAD_SIZE!);
 
 // JWT
 export const jwtSecret: string = process.env.JWT_SECRET || "";
