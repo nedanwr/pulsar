4e304d1eb3e9918b60850c3d022c64454d0b6b48
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-29 12:59:06 +0200

    Refactor: hard code fileSize & change to 10MB

--- src/utils/initMulter.ts
+++ src/utils/initMulter.ts
@@ -1,14 +1,13 @@
 import { Request } from "express";
 import multer, { FileFilterCallback } from "multer";
-import { convertBytes } from "@lib/convertBytes";
 import { checkFileType } from "@lib/checkFileType";
 
 export const upload = multer({
     storage: multer.memoryStorage(),
     limits: {
-        fileSize: convertBytes(), // 5 MB
+        fileSize: 10000000 // 5 MB
     },
-    fileFilter: async (_req: Request, file: Express.Multer.File, callback: FileFilterCallback) => {
-        await checkFileType(file, callback);
+    fileFilter(_req: Request, file: Express.Multer.File, callback: FileFilterCallback) {
+        checkFileType(file, callback);
     }
 });
