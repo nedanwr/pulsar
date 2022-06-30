066e652c3a8ff0d9dafcb2ea52d09e8c4e2452c5
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-30 21:52:33 +0200

    Feat: add modify server controller

--- src/routes/servers.ts
+++ src/routes/servers.ts
@@ -3,10 +3,14 @@ import { verifyToken } from "@utils/verifyToken";
 const router: Router = Router();
 
 // Import Controllers
-import { createServer } from "@controllers/servers";
+import { createServer, modifyServer } from "@controllers/servers";
 
-// @desc        Create a server
+// @desc        Create Server
 // @route       POST /api/v1/server
 router.route("/").post(verifyToken, createServer);
+
+// @desc        Modify Server
+// @route       PUT /api/v1/server/:id
+router.route("/:id").patch(verifyToken, modifyServer);
 
 export default router;
