a5e9c3391c645bac9a2d5ecfb69b9c223b786ca8
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-30 16:54:37 +0200

    Feat: return partial server object on success

--- src/controllers/servers/createServer.ts
+++ src/controllers/servers/createServer.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import Joi, { ObjectSchema, ValidationResult } from "joi";
 import { prisma } from "@prisma";
+import type { Server } from "@prisma/client";
 import { decodeToken } from "@utils/decodeToken";
 
 const createServer = async (req: Request, res: Response) => {
@@ -38,15 +39,25 @@ const createServer = async (req: Request, res: Response) => {
             createdAt: new Date().getTime(),
         }
     })
+        .then((server: Server) => {
+            return res.status(201).json({
+                statusCode: 201,
+                message: "Server created successfully",
+                server: {
+                    id: server.id,
+                    name: server.name,
+                    description: server.description,
+                    owner_id: server.owner_id,
+                    icon_url: server.icon_url,
+                    createdAt: server.createdAt,
+                }
+            });
+        })
         .catch((error: Error | null) => {
             throw error;
         })
         .finally(async () => {
             await prisma.$disconnect();
-            return res.status(201).json({
-                statusCode: 201,
-                message: "Server created successfully",
-            });
         });
 };
 
