b9b3748f145093909d638c83763730ff2181ee3a
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-30 16:15:08 +0200

    Fix: reduce window to 15 days as 6 months does not fit a 32-bit signed int

--- src/utils/createUserLimiter.ts
+++ src/utils/createUserLimiter.ts
@@ -1,9 +1,9 @@
 import rateLimit from "express-rate-limit";
 
 export const createUserLimiter = rateLimit({
-    windowMs: Math.floor(1.577e+10),
+    windowMs: Math.round(1.296e+9),
     max: 5,
-    message: "Too many accounts created from this IP, please try again after 6 months",
+    message: "Too many accounts created from this IP, please try again after 15 days",
     standardHeaders: true,
     legacyHeaders: false,
 });
