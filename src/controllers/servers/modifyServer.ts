6cf9d5cfaaab3318f871f12cda9c49c37291424d
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-30 20:54:00 +0200

    Feat: validate request body & if error, send status 400

--- src/controllers/servers/modifyServer.ts
+++ src/controllers/servers/modifyServer.ts
@@ -23,6 +23,37 @@ const modifyServer = async (req: Request, res: Response) => {
             error: "Bad Request",
             message: "Invalid server id",
         });
+
+    // Upload the icon to memory storage
+    upload.single("icon")(req, res, async (error: any) => {
+        if (error) throw error;
+
+        // Validate request body
+        const schema: ObjectSchema<any> = Joi.object({
+            username: Joi.string()
+                .min(5)
+                .max(16)
+                .optional(),
+
+            description: Joi.string()
+                .min(5)
+                .max(128)
+                .optional(),
+
+            icon: Joi.optional(),
+        });
+
+        const result: ValidationResult<object> = schema.validate(req.body);
+
+        // If the request body is invalid, return a 400 error
+        if (result.error)
+            return res.status(400)
+                .json({
+                    statusCode: 400,
+                    error: "Bad Request",
+                    message: result.error?.details[0].message,
+                });
+    });
 }
 
 export default modifyServer;
