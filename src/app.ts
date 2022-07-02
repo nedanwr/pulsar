a3ae748ec1f262efc67ddf027f94f5a258a69e21
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-07-02 21:04:19 +0200

    Feat: use error handler for handling basic errors

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,6 @@
 import "./start";
+import { errorHandler } from "./errorHandler";
+
+process.on("unhandledRejection", errorHandler);
+process.on("uncaughtException", errorHandler);
+process.on("uncaughtExceptionMonitor", errorHandler);
