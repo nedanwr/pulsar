03d7b6a55ffd11d591f04d3e128220f192b393e4
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-07-03 12:21:21 +0200

    Feat: check if channel with channel id provided exists

--- src/controllers/messages/createMessage.ts
+++ src/controllers/messages/createMessage.ts
@@ -15,6 +15,16 @@ const createMessage = async (req: Request, res: Response) => {
                 message: "Channel id is required in params"
             });
 
+    // Check if channel exists
+    if (!await channelExists(channelId))
+        // If channel does not exist, return 404 status
+        return res.status(404)
+            .json({
+                statusCode: 404,
+                error: "Not Found",
+                message: `Channel with id "${channelId}" not found`
+            });
+
     // Validate the request body
     const schema: ObjectSchema = Joi.object({
         content: Joi.string()
