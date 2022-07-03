702e17898174abb07e5f3f6091893870fa513d16
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-07-03 12:07:52 +0200

    Feat: import & use channelRoutes

--- src/routes/servers.ts
+++ src/routes/servers.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { verifyToken } from "@utils/verifyToken";
 const router: Router = Router();
+
+// Import Middleware
+import { verifyToken } from "@utils/verifyToken";
 
 // Import Controllers
 import { createServer, modifyServer } from "@controllers/servers";
+
+// Import Routes
+import channelRoutes from "./channels";
 
 // @desc        Create Server
 // @route       POST /api/v1/server
@@ -12,5 +17,8 @@ router.route("/").post(verifyToken, createServer);
 // @desc        Modify Server
 // @route       PUT /api/v1/server/:id
 router.route("/:id").patch(verifyToken, modifyServer);
+
+// Use Channel Routes
+router.use("/:server_id/channels", channelRoutes);
 
 export default router;
