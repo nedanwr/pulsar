7289c79515f9695a942f1825f1082515a0e490b2
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-07-03 12:03:56 +0200

    Feat: check if server with server id provided exists

--- src/controllers/channels/createChannel.ts
+++ src/controllers/channels/createChannel.ts
@@ -15,6 +15,15 @@ const createChannel = async (req: Request, res: Response) => {
             error: "Bad Request",
             message: "Server id is required in params"
         });
+
+    // Check if server exists
+    if (!await serverExists(serverId))
+        // If server does not exist, return 404 status
+        return res.status(404).json({
+            statusCode: 404,
+            error: "Not Found",
+            message: `Server with id "${serverId}" not found`
+        });
 }
 
 export default createChannel;
