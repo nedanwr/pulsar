dc65f298abae26fa1ec3766d2cf80a16f6aad090
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-07-04 22:43:35 +0200

    Feat: compare password & if doesn't match send 401 status

--- src/controllers/users/deleteCurrentUser.ts
+++ src/controllers/users/deleteCurrentUser.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
 import Joi, { ObjectSchema, ValidationResult } from "joi";
+import { prisma } from "@prisma";
+import { compare } from "bcryptjs";
+
+// Import Utils
+import { decodeToken } from "@utils/decodeToken";
 
 const deleteCurrentUser = async (req: Request, res: Response) => {
     // Validate request body
@@ -18,6 +23,39 @@ const deleteCurrentUser = async (req: Request, res: Response) => {
                 error: "Bad Request",
                 message: result.error.details[0].message,
             });
+
+    // Decode the token
+    const token: any = await decodeToken(req);
+
+    // Find the user
+    const user = await prisma.user.findFirst({
+        where: {
+            id: token.uid,
+        }
+    });
+
+    // If user doesn't exist, return 500 error
+    if (!user)
+        return res.status(500)
+            .json({
+                statusCode: 500,
+                error: "Internal Server Error",
+                message: "User not found",
+            });
+
+    // If password is incorrect, return 401 error
+    compare(req.body.password, user.password, (error: Error, isMatch: boolean) => {
+        if (error) throw error;
+
+        // If the password doesn't match, return 401 error
+        if (!isMatch)
+            return res.status(401)
+                .json({
+                    statusCode: 401,
+                    error: "Unauthorized",
+                    message: "Incorrect password",
+                });
+    });
 }
 
 export default deleteCurrentUser;
