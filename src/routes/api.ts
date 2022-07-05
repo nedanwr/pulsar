3b5600c01fc909de61ee850c470befc6c12d9fe6
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-07-05 12:15:43 +0200

    Feat: import & use message routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -15,6 +15,7 @@ import registerRoute from "@routes/auth/user";
 import loginRoute from "@routes/auth";
 import userRoutes from "@routes/users";
 import serverRoutes from "@routes/servers";
+import messageRoutes from "@routes/messages";
 
 // Use Routes
 router.use("/docs", swagger.serve);
@@ -23,5 +24,6 @@ router.use("/auth/user", createUserLimiter, registerRoute);
 router.use("/auth", loginRoute);
 router.use("/users", userRoutes);
 router.use("/servers", serverRoutes);
+router.use("/channels/:channel_id/messages", messageRoutes);
 
 export default router;
